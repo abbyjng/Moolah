a6a7276b00fbe51f3460adc03f1ede47ad2908fc
abbyjng <abigailjng@gmail.com>
2021-09-20 22:11:37 +0000

    Fixed small error in constants.js

--- constants.js
+++ constants.js
@@ -1,6 +1,6 @@
 module.exports = {
-  MAX_USERS = 12,
-  MAX_DESCRIPTION = 100,
-  MAX_COST = 1000,
-  MAX_OWE = 10000
+  MAX_USERS: 12,
+  MAX_DESCRIPTION: 100,
+  MAX_COST: 1000,
+  MAX_OWE: 10000,
 };
